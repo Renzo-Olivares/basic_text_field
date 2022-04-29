c3129dac378d75dd9d67de0e912c520c8aaa07ad
Renzo Olivares <roliv@google.com>
2022-04-29 11:19:10 -0700

    updates

--- lib/basic_text_input_client.dart
+++ lib/basic_text_input_client.dart
@@ -134,6 +134,8 @@ class BasicTextInputClientState extends State<BasicTextInputClient>
       return;
     }
 
+    final bool selectionChanged = _value.selection != value.selection;
+
     widget.updateTextEditingDeltaHistory?.call(textEditingDeltas);
 
     _value = value;
@@ -143,6 +145,8 @@ class BasicTextInputClientState extends State<BasicTextInputClient>
         (widget.controller as ReplacementTextEditingController).syncReplacementRanges(delta);
       }
     }
+
+    if (selectionChanged) widget.onSelectionChanged(value.selection, SelectionChangedCause.keyboard);
   }
 
   @override
